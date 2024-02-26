0b67f27828003742510b4f33691d5f82c48ad8f4
Alexander Egorov <egoroff@gmail.com>
2024-02-26 11:44:43 +0300

    chore: deprecated builtin.eslint removed

--- nvim/lua/custom/configs/null-ls.lua
+++ nvim/lua/custom/configs/null-ls.lua
@@ -3,7 +3,6 @@ local null_ls = require("null-ls")
 
 local opts = {
   sources = {
-    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.clang_format,
     null_ls.builtins.formatting.shfmt,
