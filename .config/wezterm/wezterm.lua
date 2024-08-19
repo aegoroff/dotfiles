d234fea8db52c1e2fb7ba78545302b6ce26e603b
Alexander Egorov <egoroff@gmail.com>
2024-08-19 19:55:38 +0300

    chore: wezterm theme changed

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,9 +6,9 @@ config.font_size = 12
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.enable_wayland = false
 --config.color_scheme = 'Violet Light'
-config.color_scheme = 'Alabaster'
+--config.color_scheme = 'Alabaster'
 --config.color_scheme = 'Selenized Dark (Gogh)'
---config.color_scheme = 'Selenized Light (Gogh)'
+config.color_scheme = 'Selenized Light (Gogh)'
 --config.color_scheme = 'Everforest Light Hard (Gogh)'
 --config.color_scheme = 'Everforest Light Soft (Gogh)'
 --config.color_scheme = 'Catppuccin Latte'
