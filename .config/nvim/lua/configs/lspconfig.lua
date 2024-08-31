6d9da8af6f98f3f67ebe2e1a88cd921e6172650b
Alexander Egorov <egoroff@gmail.com>
2024-09-01 00:13:11 +0300

    fix: explicit rust_analyzer setup removed

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "gopls", "vuels", "angularls", "zls", "bashls", "rust_analyzer" }
+local servers = { "html", "cssls", "gopls", "vuels", "angularls", "zls", "bashls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
