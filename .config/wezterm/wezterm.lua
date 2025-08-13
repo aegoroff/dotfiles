2f7f345a342b2f3ee8e949289f7793647605d986
Alexander Egorov <egoroff@gmail.com>
2025-08-13 09:03:10 +0300

    chore: commented config code removed

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,17 +5,7 @@ config.front_end = "WebGpu"
 config.font_size = 12
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.enable_wayland = false
---config.color_scheme = 'Violet Light'
 config.color_scheme = 'Alabaster'
---config.color_scheme = 'Selenized Dark (Gogh)'
---config.color_scheme = 'Selenized Light (Gogh)'
---config.color_scheme = 'Vs Code Light+ (Gogh)'
---config.color_scheme = 'Everforest Light Hard (Gogh)'
---config.color_scheme = 'Everforest Light Soft (Gogh)'
---config.color_scheme = 'Catppuccin Latte'
---config.color_scheme = 'Catppuccin Frappe'
---config.color_scheme = 'Tokyo Night'
---config.color_scheme = 'MaterialDarker'
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 --config.freetype_load_target = "HorizontalLcd"
 --config.freetype_render_target = 'HorizontalLcd'
