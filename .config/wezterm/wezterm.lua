3de63cd6abf234e6876ef872afe4a22952f29ef0
Alexander Egorov <egoroff@gmail.com>
2025-08-14 20:13:01 +0300

    chore: wezterm color scheme set to google light base 16

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ config.front_end = "WebGpu"
 config.font_size = 12
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.enable_wayland = false
-config.color_scheme = 'Material'
+config.color_scheme = 'Google Light (base16)'
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 --config.freetype_load_target = "HorizontalLcd"
 --config.freetype_render_target = 'HorizontalLcd'
