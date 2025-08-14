8acdc4a8731c27a0c4557706c2c8f2b7e7df5ff9
Alexander Egorov <egoroff@gmail.com>
2025-08-14 19:58:56 +0300

    chore: wezterm theme changed to material

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ config.front_end = "WebGpu"
 config.font_size = 12
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.enable_wayland = false
-config.color_scheme = 'Alabaster'
+config.color_scheme = 'Material'
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 --config.freetype_load_target = "HorizontalLcd"
 --config.freetype_render_target = 'HorizontalLcd'
