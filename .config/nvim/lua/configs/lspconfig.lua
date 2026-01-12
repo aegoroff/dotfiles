71df5ac7eb75804dc41f753f587907cd4deeda42
Alexander Egorov <egoroff@gmail.com>
2026-01-12 22:35:30 +0300

    fix: clang LSP server configuration corrected

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -59,6 +59,6 @@ vim.lsp.config('clangd', {
     "--clang-tidy",
     "--header-insertion=iwyu",
     "--completion-style=detailed",
-    "--function-arg-placeholders",
+    "--function-arg-placeholders=1",
   },
 })
